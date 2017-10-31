e9a7d94fd38690d0c85ab77c378fc5d337692907
Alejandro <alejandro.arboleda@ceiba.com.co>
2017-10-31 12:13:51 -0500

    creacion clases version 5

--- Parqueadero/src/main/java/Parqueadero/clases/good/be/alejo/Portero.java
+++ Parqueadero/src/main/java/Parqueadero/clases/good/be/alejo/Portero.java
@@ -1,5 +1,29 @@
 package Parqueadero.clases.good.be.alejo;
 
 public class Portero {
+	
+	private int identificador;
+	private Parqueadero parqueadero;
+	
+	
+	/**
+	 * la funcion recive un identificador del vehiculo y luego registra su ingreso al parqueadero
+	 * @param placa identificador del vehiculo
+	 */
+	public void registrarIngresoCarro(String placa) {
+		
+	}
+	
+	public void registrarSalidaCarro(String placa) {
+		
+	}
+	
+	public boolean comprobarDisponibilidadParquederoCarros(){
+		return true;
+	}
+	
+	public double calcularValorParqueo() {
+		return 0.0;
+	}
 
 }
